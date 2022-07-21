fddfa68c7b3d52e38f1fc2ef6fdb5815804791d0
Author <43812455+Userrnam@users.noreply.github.com>
2022-07-21 19:23:36 +0200

    add 1s delay in printer

--- src/printer.c
+++ src/printer.c
@@ -1,5 +1,6 @@
 #include "printer.h"
 
+#include <time.h>
 #include <stdio.h>
 
 #include "array.h"
@@ -9,13 +10,33 @@ ARRAY_BASIC_TYPE(float)
 void *printer(printer_params_t *params) {
 	ring_buffer_t *analyzer_printer_buffer = params->analyzer_printer_buffer;
 
+	int count = 0;
+	float_array_t *total = NULL;
+	time_t prev_time = time(NULL);
 	while (*params->is_running) {
 		float_array_t *cpu_usage = read_packet(analyzer_printer_buffer);
+		if (!total) {
+			total = alloc_float_array(cpu_usage->count);
+			memset(total->elems, 0, sizeof(float) * (size_t)total->count);
+		}
 
-		// beginning is the escape sequence to clear the screen
-		puts("\033[2JCPU usage:");
-		for (int i = 0; i < cpu_usage->count; ++i) {
-			printf("core %d: %f\n", i, (double)cpu_usage->elems[i]);
+		// update 'per frame' values
+		count++;
+		for (int i = 0; i < total->count; ++i) {
+			total->elems[i] += cpu_usage->elems[i];
+		}
+
+		if (time(NULL) - prev_time >= 1) {
+			// print usage, beginning is the escape sequence to clear the screen
+			puts("\033[1;1H\033[2JCPU usage:");
+			for (int i = 0; i < total->count; ++i) {
+				printf("core %d: %f\n", i, (double)total->elems[i] / (double)count);
+			}
+
+			// reset 'per frame' values
+			prev_time = time(NULL);
+			count = 0;
+			memset(total->elems, 0, sizeof(float) * (size_t)total->count);
 		}
 
 		free(cpu_usage);
