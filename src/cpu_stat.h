095840bcbcdbca7dc0cfd1a0d5aab2b26cdbd2ca
Author <43812455+Userrnam@users.noreply.github.com>
2022-07-21 17:23:03 +0200

    move cpu_stat into separate file

---
+++ src/cpu_stat.h
@@ -0,0 +1,20 @@
+#ifndef CPU_STAT_H
+#define CPU_STAT_H
+
+typedef struct cpu_stat {
+	int user;
+	int nice;
+	int system;
+	int idle;
+	int iowait;
+	int irq;
+	int softirq;
+	int steal;
+	int guest;
+	int guest_nices;
+} cpu_stat_t;
+
+ARRAY(cpu_stat);
+
+
+#endif // CPU_STAT_H
