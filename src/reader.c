563e449c1d7386b8e0078bc6e78b67cc67e1b666
Author <43812455+Userrnam@users.noreply.github.com>
2022-07-23 12:10:45 +0200

    move parsing cpu stats to separate function

--- src/reader.c
+++ src/reader.c
@@ -5,54 +5,56 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "cpu_stat.h"
-
 #define BUFFER_SIZE 2048
+
+cpu_stat_array_t *parse_cpu_stats(char *s) {
+	// get cpu count.
+	int core_count = 0;
+	char *p = s;
+	while (1) {
+		p = strchr(p, '\n')+1;
+		if (memcmp(p, "cpu", 3) == 0) {
+			core_count++;
+		} else {
+			break;
+		}
+	}
+
+	// parse cpu stats
+	cpu_stat_array_t *cpu_stats = alloc_cpu_stat_array(core_count);
+	p = s;
+	for (int i = 0; i < core_count; ++i) {
+		p = strchr(p, '\n')+1;
+		sscanf(p, "%*s %d %d %d %d %d %d %d %d %d %d",
+				&cpu_stats->elems[i].user,
+				&cpu_stats->elems[i].nice,
+				&cpu_stats->elems[i].system,
+				&cpu_stats->elems[i].idle,
+				&cpu_stats->elems[i].iowait,
+				&cpu_stats->elems[i].irq,
+				&cpu_stats->elems[i].softirq,
+				&cpu_stats->elems[i].steal,
+				&cpu_stats->elems[i].guest,
+				&cpu_stats->elems[i].guest_nices);
+	}
+	return cpu_stats;
+}
 
 void *reader(reader_params_t *params) {
 	FILE *fp = fopen("/proc/stat", "r");
 
 	ring_buffer_t *reader_analyzer_buffer = params->reader_analyzer_buffer;
 
-	// contents of file /proc/stat is constantly updating, so we copy should copy it into a
+	// contents of file /proc/stat is constantly updating, so we copy it into a
 	// buffer before processing.
 	char buf[BUFFER_SIZE];
 
-	// 0 means uninitialized
-	int cpu_count = 0;
 	while (*params->is_running) {
 		memset(buf, 0, BUFFER_SIZE);
 		rewind(fp);
 		fread(buf, 1, BUFFER_SIZE-1, fp);
 
-		if (cpu_count == 0) {
-			// get cpu count
-			char *p = buf;
-			while (1) {
-				p = strchr(p, '\n')+1;
-				if (memcmp(p, "cpu", 3) == 0) {
-					cpu_count++;
-				} else {
-					break;
-				}
-			}
-		}
-		cpu_stat_array_t *packet = alloc_cpu_stat_array(cpu_count);
-		char *p = buf;
-		for (int i = 0; i < cpu_count; ++i) {
-			p = strchr(p, '\n')+1;
-			sscanf(p, "%*s %d %d %d %d %d %d %d %d %d %d",
-					&packet->elems[i].user,
-					&packet->elems[i].nice,
-					&packet->elems[i].system,
-					&packet->elems[i].idle,
-					&packet->elems[i].iowait,
-					&packet->elems[i].irq,
-					&packet->elems[i].softirq,
-					&packet->elems[i].steal,
-					&packet->elems[i].guest,
-					&packet->elems[i].guest_nices);
-		}
+		cpu_stat_array_t *packet = parse_cpu_stats(buf);
 
 		write_packet(reader_analyzer_buffer, packet);
 
--- src/reader.h
+++ src/reader.h
@@ -3,11 +3,14 @@
 
 #include "ring_buffer.h"
 
+#include "cpu_stat.h"
+
 typedef struct reader_params {
     ring_buffer_t *reader_analyzer_buffer;
     volatile int *is_running;
 } reader_params_t;
 
+cpu_stat_array_t *parse_cpu_stats(char *s);
 void *reader(reader_params_t *params);
 
 #endif // READER_H
